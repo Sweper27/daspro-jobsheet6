2ba7f394e2ee034bb1ba0e49890c53ad16e1ccf0
Sweper27 <rifqiariessaputra@gmail.com>
2024-10-03 07:54:17 +0700

    memasukkan percobaan 1

---
+++ pemilihan2percobaan1No26.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class pemilihan2percobaan1No26 {
+
+    public static void main(String[] args) {
+        Scanner input26 = new Scanner(System.in);
+
+        System.out.print("Masukkan tahun: ");
+        int tahun = input26.nextInt();
+
+        if ((tahun % 4) == 0) {
+            if ((tahun % 100) != 0) 
+                System.out.println("Tahun Kabisat");
+        }else {
+            System.out.println( " bukan tahun kabisat.");
+        }
+
+
+    } 
+}
