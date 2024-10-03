2fb8b209928254eb9f3354c8040f2a69998c974f
Sweper27 <rifqiariessaputra@gmail.com>
2024-10-03 10:04:57 +0700

    memodifikasi untuk soal nomer 4

--- pemilihan2percobaan1No26.java
+++ pemilihan2percobaan1No26.java
@@ -9,13 +9,16 @@ public class pemilihan2percobaan1No26 {
         int tahun = input26.nextInt();
 
         if ((tahun % 4) == 0) {
-            if ((tahun % 100) != 0) {
+            if ((tahun % 100) == 0) {
+                if ((tahun % 400)== 0 ) {
                 System.out.println("Tahun Kabisat");
-            }else 
-                System.out.println( " bukan tahun kabisat.");
-        }else {  
-            System.out.println("bukan tahun kabisat");
-            }
+                 }else 
+                    System.out.println( " bukan tahun kabisat.");
+            }else {  
+                System.out.println("bukan tahun kabisat");
+        }}else{
+            System.out.println("Bukan Tahun Kabisat");
+    }
 
 
     } 
